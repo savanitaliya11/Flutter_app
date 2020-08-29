bab3c790466b9bfa9aa83878ae121ad70dfc4270
Savan <savanitaliya11@gmail.com>
2020-08-30 04:20:53 +0530

    Container

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -4,12 +4,14 @@ class Home extends StatelessWidget {
   @override
 
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.green,
-      // width: 200.0, it doesn't consider height and width because it only consider of it's parent and it's parent is MaterialApp.
-      // height: 100.0,
-      child: Text('Hello',textDirection: TextDirection.rtl),
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.green,
+        width: 200.0,
+        height: 100.0,
+        child: Text('Hello',textDirection: TextDirection.rtl),
+      ),
     );
   }
 }
