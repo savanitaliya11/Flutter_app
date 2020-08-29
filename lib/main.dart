4488941eaca1ed88f33fbe5a8c2e7bf1b90b85e8
Savan <savanitaliya11@gmail.com>
2020-08-29 23:20:01 +0530

    Center and Text widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,5 @@ void main(){
   //Purpose: Inflate the widget and attach to the screen.
   //runApp:shows whatever we pass in parameter.
   //Center and Text are widget.
-  runApp(
-       MaterialApp(
-        title: 'Flutter App',
-        home:  Center(
-          child: Material(
-            color: Colors.blueGrey,
-            child:Center(
-                child: Text(
-                  'Hello Savan',
-                  textDirection: TextDirection.ltr
-                ),
-            ),
-          ),
-         )
-
-      )
-  );
-
+  runApp(Text('Hello Flutter',textDirection: TextDirection.ltr,));
 }
