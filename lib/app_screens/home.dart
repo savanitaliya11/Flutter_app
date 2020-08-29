cbc8b89fe7b014b754fbb37e6b27d2cc8afac699
Savan <savanitaliya11@gmail.com>
2020-08-30 04:36:00 +0530

    Container

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -7,10 +8,14 @@ class Home extends StatelessWidget {
     return Center(
       child: Container(
         alignment: Alignment.center,
-        color: Colors.green,
-        width: 200.0,
-        height: 100.0,
-        child: Text('Hello',textDirection: TextDirection.rtl),
+        color: Colors.blue,
+         width: 200.0,
+         height: 100.0,
+        //margin: EdgeInsets.all(20),
+        //margin: EdgeInsets.only(left: 20,right: 20, top: 20, bottom: 20),
+        //padding: EdgeInsets.all(10),
+        //padding: EdgeInsets.only(left: 15, right: 15, top: 15, bottom: 15),
+        child: Text('Hello',textDirection: TextDirection.rtl,),
       ),
     );
   }
