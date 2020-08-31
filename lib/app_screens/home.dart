a514131feda7de4be5f08e71527b10bb63a2ad02
Savan <savanitaliya11@gmail.com>
2020-08-31 23:30:09 +0530

    Image asset

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../main.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -12,6 +13,7 @@ class Home extends StatelessWidget {
       ),
       body: Center(
         child: Container(
+          padding: EdgeInsets.only(left: 10,  top: 10, right: 60),
             alignment: Alignment.center,
 
             // width: 200.0,
@@ -20,49 +22,86 @@ class Home extends StatelessWidget {
             //margin: EdgeInsets.only(left: 20,right: 20, top: 20, bottom: 20),
             //padding: EdgeInsets.all(10),
             //padding: EdgeInsets.only(left: 15, right: 15, top: 15, bottom: 15),
-            child:
-
-
-            Row(
+            child:Column(
               children: [
+                Row(
+                  children: [
 
-                Expanded(child: Text(
-                  'Hybrid',
-                  textDirection: TextDirection.rtl,
-                  style: TextStyle(
-                      decoration: TextDecoration.none,
-                      fontSize: 30,
-                      fontFamily: 'Raleway',
-                      fontWeight: FontWeight.w700,
-                      fontStyle: FontStyle.italic,
-                      color: Colors.red),
-                )),
+                    Expanded(child: Text(
+                      'Hybrid',
+                      textDirection: TextDirection.rtl,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 30,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.blue
+                      ),
+                    )),
 
-                Expanded(child: Text(
-                  'Flutter',
-                  textDirection: TextDirection.rtl,
-                  style: TextStyle(
-                      decoration: TextDecoration.none,
-                      fontSize: 30,
-                      fontFamily: 'Raleway',
-                      fontWeight: FontWeight.w700,
-                      fontStyle: FontStyle.italic,
-                      color: Colors.red),
-                )),
-                Expanded(child: Text(
-                  'Native',
-                  textDirection: TextDirection.rtl,
-                  style: TextStyle(
-                      decoration: TextDecoration.none,
-                      fontSize: 30,
-                      fontFamily: 'Raleway',
-                      fontWeight: FontWeight.w700,
-                      fontStyle: FontStyle.italic,
-                      color: Colors.red),
-                )),
+                    Expanded(child: Text(
+                      'Flutter',
+                      textDirection: TextDirection.rtl,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 30,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.blue),
+                    )),
+
+                  ],
+                ),
+                Row(
+                  children: [
+
+                    Expanded(child: Text(
+                      'Hybrid',
+                      textDirection: TextDirection.rtl,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 30,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.blue),
+                    )),
+
+                    Expanded(child: Text(
+                      'Flutter',
+                      textDirection: TextDirection.rtl,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 30,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.blue),
+                    )),
+
+                  ],
+                ),
+
+                FlutterImageAsset()
               ],
-            )),
+            )
+
+
+           ),
       ),
     );
   }
+}
+
+class FlutterImageAsset extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+  AssetImage assetImage = AssetImage('images/flutter.png');
+  Image image = Image(image: assetImage,);
+  return Container(
+    child: image,
+  );
+}
 }
